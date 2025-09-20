6ce7885550a7fc81b7f9b80de1c01f836b45735c
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 15:46:43 -0300

    Update Ejercicio3.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio3.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio3.asm
@@ -1,4 +1,33 @@
-a)- La principal diferencia que tiene INT7 con INT6 es que en BX se almacena la direccion de memoria en donde esta la variable donde queremos guardar el caracter a ingresar.
+; a)- La principal diferencia que tiene INT7 con INT6 es que en BX se almacena la direccion de memoria en donde esta la variable donde queremos guardar el caracter a ingresar.
 
-b)- La segunda instruccion INT 7 imprime el caracter que fue leido.
+; b)- La segunda instruccion INT 7 imprime el caracter que fue leido.
+
+; c)- El valor numerico almacenado en CL es el que se ingresa por teclado.
+
+; d)- 
+
+ORG 1000H
+MSJ   DB "INGRESE UN NUMERO:"
+FIN   DB ?
+
+ORG 1500H
+NUM1   DB ?
+NUM2   DB ?
+
+ORG 2000H
+  MOV BX, OFFSET MSJ
+  MOV AL, OFFSET FIN-OFFSET MSJ
+  INT 7
+  MOV BX, OFFSET NUM1
+  INT 6
+  MOV AL, 1
+  INT 7
+  MOV BX, OFFSET NUM2
+  INT 6
+  MOV AL, 1
+  INT 7
+  MOV CL, NUM1
+  MOV CH, NUM2
+  INT 0
+END
 
