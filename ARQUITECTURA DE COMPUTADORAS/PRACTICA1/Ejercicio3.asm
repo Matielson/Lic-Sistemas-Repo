839365275ca474dcaf57641a913bc55fe21e9533
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 15:40:25 -0300

    Update Ejercicio3.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio3.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio3.asm
@@ -1,1 +1,4 @@
+a)- La principal diferencia que tiene INT7 con INT6 es que en BX se almacena la direccion de memoria en donde esta la variable donde queremos guardar el caracter a ingresar.
+
+b)- La segunda instruccion INT 7 imprime el caracter que fue leido.
 
