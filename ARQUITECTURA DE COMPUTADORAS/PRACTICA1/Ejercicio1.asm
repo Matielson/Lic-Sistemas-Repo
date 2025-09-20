ddaddbad276535e4c933391120d5fa2f94843d8c
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 13:47:07 -0300

    Update and rename Ejercicio1.txt to Ejercicio1.asm

---
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio1.asm
@@ -0,0 +1,21 @@
+        org 1000h
+msj     DB "la puta que te pario" ;1000h-1003h
+cant    DB 0
+        org 2000h
+        MOV AL,'a'
+        MOV BX, offset msj
+        MOV DX, offset cant - offset msj
+        
+bucle:  CMP BL, DL
+        JZ fin
+        CMP [BX], AL
+        JNZ seguir
+        INC cant
+        
+seguir: INC BX
+        JMP bucle
+
+        
+fin:    MOV CL, cant
+        HLT
+end
