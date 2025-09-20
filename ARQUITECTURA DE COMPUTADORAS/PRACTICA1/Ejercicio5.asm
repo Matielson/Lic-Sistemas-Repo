6056e60969d86734b9a6e0b956da0625e61258a4
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 20:53:52 -0300

    Update Ejercicio5.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio5.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio5.asm
@@ -5,7 +5,7 @@ letra  DB "A"
 bucle: MOV BX, offset letra
        INT 7
        INC letra
-       CMP byte ptr[offset letra],'Z'
+       CMP byte ptr[offset letra],'Z'+1
        JNZ bucle
        INT 0
 END
