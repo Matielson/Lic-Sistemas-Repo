709aa56fdf30b79043b051bdedd9500917c31117
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 21:27:07 -0300

    Update Ejercicio5.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio5.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio5.asm
@@ -1,3 +1,5 @@
+# EJERCICIO A:
+
        ORG 1000H
 letra  DB "A"
        ORG 2000H
@@ -9,3 +11,50 @@ bucle: MOV BX, offset letra
        JNZ bucle
        INT 0
 END
+
+# EJERCICIO B
+; Tendria que modificar el valor de la variable letra (la inicial) y la condicion de fin Z y poner 9
+
+       ORG 1000H
+letra  DB "0"
+       ORG 2000H
+       MOV AL, 1
+bucle: MOV BX, offset letra
+       INT 7
+       INC letra
+       CMP byte ptr[offset letra],'9'+1
+       JNZ bucle
+       INT 0
+END
+
+# EJERCICIO C
+       ORG 1000H
+letra  DB ?
+       ORG 2000H
+       MOV letra, 32
+       MOV AL, 1
+bucle: MOV BX, offset letra
+       INT 7
+       INC letra
+       CMP byte ptr[offset letra],127
+       JNZ bucle
+       INT 0
+END
+
+# EJERCICIO D
+       ORG 1000H
+letra  DB "0"
+       ORG 2000H
+       MOV AL, 1
+bucle: MOV BX, offset letra
+       INT 7
+       INC letra
+       MOV BX, 10
+       INT 7
+       CMP byte ptr[offset letra],'9'+1
+       JNZ bucle
+       INT 0
+END
+
+
+
