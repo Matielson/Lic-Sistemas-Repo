4fd31967e6298cf309a8b71bdfe606dfedc50e06
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-20 21:58:51 -0300

    Update Ejercicio6.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio6.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA1/Ejercicio6.asm
@@ -1,3 +1,5 @@
+# INCISO A
+
        ORG 1000H
 letra  DB ?
 cant   DB 0
@@ -8,8 +10,33 @@ bucle: MOV BX, offset letra
        INT 6
        INC letra
        INC cant
-       CMP byte ptr[offset letra],'.'
+       CMP byte ptr[offset letra],'.'+1
        JNZ bucle
        MOV DL, cant
        INT 0
 END
+
+# INCISO B
+
+       ORG 1000H
+letra  DB ?
+cant   DB 0
+       ORG 2000H
+       MOV letra, 32
+       MOV AL, 1
+       
+bucle: MOV BX, offset letra
+       INT 6
+       CMP byte ptr[offset letra],'a'
+       JNZ NoEsA
+       INC cant
+
+NoEsA: INC letra
+       CMP byte ptr[offset letra],'.'+1
+       JNZ bucle
+       MOV DL, cant
+       INT 0
+END
+
+# INCISO C
+
