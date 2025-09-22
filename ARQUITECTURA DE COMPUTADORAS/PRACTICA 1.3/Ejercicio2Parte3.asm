d5d0b7d0ee1bab1589bb8dbb0cbad0b692f8202d
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-22 16:23:44 -0300

    Update Ejercicio2Parte3.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA 1.3/Ejercicio2Parte3.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA 1.3/Ejercicio2Parte3.asm
@@ -5,11 +5,13 @@ A      DB 8
 B      DB 5
 C      DB 4
 D      DB ?
+
        ORG 3000h
 CALC:  ADD DL, AL
        ADD DL, AH
        SUB DL, CL
        RET
+
        ORG 2000h
        MOV AL, A
        MOV AH, B
@@ -18,3 +20,46 @@ CALC:  ADD DL, AL
        MOV D, DL
        INT 0
 END
+
+
+# INCISO B
+
+       ORG 1000h
+A      DB 8
+B      DB 5
+C      DB 4
+D      DB ?
+
+       ORG 3000h
+CALC:  PUSH BX
+       MOV BX, SP
+       SUB BX, 8
+       MOV DL, [BX]
+       SUB BX, 2
+       ADD DL, [BX]
+       SUB BX, 2
+       SUB DL, [BX]
+       POP BX
+       RET
+
+       ORG 2000h
+       MOV AL, A
+       PUSH AX
+       MOV AL, B
+       PUSH AX
+       MOV AL, C
+       PUSH AX
+       CALL CALC
+       MOV D, DL
+       POP AX
+       POP AX
+       POP AX
+       INT 0
+END
+
+# INCISO C
+
+TERMINAR.
+
+
+
