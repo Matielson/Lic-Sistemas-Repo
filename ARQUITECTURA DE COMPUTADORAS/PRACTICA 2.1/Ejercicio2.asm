e27ba76d55a1b2ec14ebb7af7054bf8b816323fa
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-27 14:56:16 -0300

    Update Ejercicio2.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.1/Ejercicio2.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.1/Ejercicio2.asm
@@ -10,4 +10,36 @@ OUT PB, AL
 HLT
 END
 
+
 # INCISO B
+
+        PA EQU 30h
+        PB EQU 31h
+        CA EQU 32h
+        CB EQU 33h
+        
+        ORG 1000h
+msj     DB "Llave Prendida"
+msj2    DB "Llave Apagada"
+        ORG 2000h
+        MOV AL, 10000000b
+        OUT CA, AL
+        MOV AL, 00000000b
+        OUT CB, AL
+        IN AL, PA
+        OUT PB, AL
+        IN AL, PB
+        CMP AL, 10000000b
+        JNZ apag
+        MOV AL, 14
+        MOV BX, offset msj
+        INT 7
+        JMP fin
+apag:   MOV AL,  13
+        MOV BX, offset msj2
+        INT 7
+FIN:    HLT
+        END
+
+
+# INCISO C
