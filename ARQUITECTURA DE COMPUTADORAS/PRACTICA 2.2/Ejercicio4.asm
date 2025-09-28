28af5f5162bb6e519c87e9cee31e2ec2807c3006
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-28 09:44:10 -0300

    Update Ejercicio4.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.2/Ejercicio4.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.2/Ejercicio4.asm
@@ -130,3 +130,84 @@ END
 
 
 # INCISO C
+
+PA EQU 30h
+PB EQU 31h
+CA EQU 32h
+CB EQU 33h
+
+ORG 1000h
+msjP    DB "El mensaje ingresado se esta escribiendo en la impresora..."
+fin2    DB ?
+msj     DB ?
+fin     DB ?
+
+
+ORG 3000h
+ini_pio:
+ ; CONFIGURO EL PA (estado) CON EL STROBE EN 0 Y EL BUSY EN 1
+ MOV AL, 11111101b
+ OUT CA, AL
+ 
+ ; CONFIGURO EL PUERTO DE DATO TODO EN SALIDA.
+ MOV AL, 00000000b
+ OUT CB, AL
+ RET
+
+poll: ; QUEDA EN BUCLE HASTA QUE SE LIBERE LA IMPRESORA
+ IN AL, PA ; Tomo el estado de la impresora
+ AND AL, 1 ; Pregunto si esta ocupada
+ JNZ poll ; Utilizando el "flag interno" salto si sigue ocupada
+ RET
+ 
+flanco_ascendente: 
+ ; MANDO EL STROBE EN 0
+ IN AL, PA
+ AND AL, 0FDh
+ OUT PA, AL
+
+ ; MANDO EL STROBE EN 1 (FLANCO ASCENDENTE)
+ IN AL, PA
+ OR AL, 2
+ OUT PA, AL
+ 
+ ; VUELVO
+ RET
+
+imprimir_caracter:
+ MOV CX, 0 ; Inicializo mi contador local
+ ; Llamo al bucle "poll" esperando que se libere
+ call poll
+ ; Se libero la impresora, mando mi caracter al puerto de datos (PB).
+ MOV AL, [BX]
+ OUT PB, AL
+ ; Realizo el flanco ascendente para que la impresora sepa que queremos imprimir
+ call flanco_ascendente
+ INC BX ; Paso al siguiente caracter
+ INC CX ; Incremento mi contador local
+ CMP byte ptr[BX], 0 ; Pregunto si llegue al final (con mi terminador (0))
+ JNZ imprimir_caracter ; Si no llegue, sigo imprimiendo
+ RET
+
+ ORG 2000h
+ MOV DX, 0 ; Inicializo mi contador de caracteres.
+ ; Ingreso un mensaje a imprimir.
+ MOV BX, offset msj
+loop: INT 6
+ CMP byte ptr[BX], '0'
+ JZ seguir
+ INC BX
+ INC DX
+ JMP loop
+ ; Mensaje a mostrar en la pantalla (msjP)
+seguir: MOV byte ptr[BX], 0 ; LE ASIGNO UN TERMINADOR A MI MENSAJE
+ MOV BX, offset msjP
+ MOV AL, offset fin2 - offset msjP
+ INT 7
+ ; Configuramos el PIO para la impresora.
+ call ini_pio
+ MOV BX, offset msj ; Movemos a BX la direccion del primer caracter del string a imprimir
+ ; Imprimimos el caracter (bucle si es un string)
+ call imprimir_caracter
+ HLT ; Termina la ejecucion
+END 
