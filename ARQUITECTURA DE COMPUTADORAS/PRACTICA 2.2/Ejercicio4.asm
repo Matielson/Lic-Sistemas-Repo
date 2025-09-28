7f4c6248060d4eaf6a8d126b14df591ed3098fdd
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-09-28 09:13:30 -0300

    Update Ejercicio4.asm

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.2/Ejercicio4.asm
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA 2.2/Ejercicio4.asm
@@ -63,3 +63,69 @@ END
 
 
 # INCISO B
+
+PA EQU 30h
+PB EQU 31h
+CA EQU 32h
+CB EQU 33h
+
+ORG 1000h
+msj     DB "ORGANIZACION Y ARQUITECTURA DE COMPUTADORAS"
+FIN     DB ? 
+msjP    DB "Un mensaje se esta escribiendo en la impresora..."
+
+ORG 3000h
+ini_pio:
+ ; CONFIGURO EL PA (estado) CON EL STROBE EN 0 Y EL BUSY EN 1
+ MOV AL, 11111101b
+ OUT CA, AL
+ 
+ ; CONFIGURO EL PUERTO DE DATO TODO EN SALIDA.
+ MOV AL, 00000000b
+ OUT CB, AL
+ RET
+
+poll: ; QUEDA EN BUCLE HASTA QUE SE LIBERE LA IMPRESORA
+ IN AL, PA ; Tomo el estado de la impresora
+ AND AL, 1 ; Pregunto si esta ocupada
+ JNZ poll ; Utilizando el "flag interno" salto si sigue ocupada
+ RET
+ 
+flanco_ascendente: 
+ ; MANDO EL STROBE EN 0
+ IN AL, PA
+ AND AL, 0FDh
+ OUT PA, AL
+
+ ; MANDO EL STROBE EN 1 (FLANCO ASCENDENTE)
+ IN AL, PA
+ OR AL, 2
+ OUT PA, AL
+ 
+ ; VUELVO
+ RET
+
+imprimir_caracter:
+ ; Llamo al bucle "poll" esperando que se libere
+ call poll
+ ; Se libero la impresora, mando mi caracter al puerto de datos (PB).
+ MOV AL, [BX]
+ OUT PB, AL
+ call flanco_ascendente
+ INC BX
+ CMP BX, offset fin
+ JNZ imprimir_caracter
+ RET
+
+ ORG 2000h
+ MOV BX, offset msjP
+ MOV AL, offset fin - offset msjP
+ INT 7
+ call ini_pio
+ MOV BX, offset msj
+ call imprimir_caracter
+ HLT
+END 
+
+
+# INCISO C
