d6e85837e2008592afd7dd89f462a06e5d1a06c8
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-10-06 20:19:56 -0300

    Update Ejercicio1.java

--- TALLER DE PROGRAMACION/PRACTICA 1/Ejercicio1.java
+++ TALLER DE PROGRAMACION/PRACTICA 1/Ejercicio1.java
@@ -4,25 +4,24 @@ import PaqueteLectura.GeneradorAleatorio;
 
 public class Ej01Tabla2 {
 
-    /**
+    /*
      * Carga un vector que representa la tabla del 2
      */
     public static void main(String[] args) {
-        GeneradorAleatorio.iniciar();
-        int DF=11;  
+        int DF = 11;  
         int [] tabla2 = new int[DF]; // indices de 0 a 10
         int i;
-        for (i=0;i<DF;i++) {
+        for (i=0;i<DF;i++) 
             tabla2[i]=2*i;
+        System.out.println("2x" + "5" + "="+ tabla2[5]);
+        
+        GeneradorAleatorio.iniciar();
+        int num = 0;
+        while (num != 11) {
+            num = GeneradorAleatorio.generarInt(12);
+            if (num != 11)
+                System.out.println (num + " multiplicado por 2 es: " +tabla2[num]);
         }
-        int num;
-        do {
-            num = GeneradorAleatorio.generarInt(12); // Genera entre 0 y 11
-            if (num < DF) {
-                System.out.println("2x" + num + " = " + tabla2[num]);
-            } else {
-                System.out.println("Número generado: " + num + " (fuera del rango de la tabla)");
-            }
-        } while (num != 11); // Termina al generar un 11
+        System.out.println("Se encontro el numero 11.");
     }
 }
