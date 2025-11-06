945fe15f388d492a445f1d01c38ab5ac459d85c8
Mati Nuñez <130598367+Matielson@users.noreply.github.com>
2025-11-06 20:47:33 -0300

    Update Ejercicio5.s

--- ARQUITECTURA DE COMPUTADORAS/PRACTICA 4.1/Ejercicio5.s
+++ ARQUITECTURA DE COMPUTADORAS/PRACTICA 4.1/Ejercicio5.s
@@ -27,8 +27,40 @@ D:    .word  0
       
 # INCISO B
 
-      .data
-A:    .word
-B:    .word
-C:    .word
-      
+            .data
+A:          .word
+B:          .word
+C:          .word
+
+            .text 
+            ld $s0, A($zero) 
+            ld $s1, B($zero) 
+            beqz $s0, AesCero 
+            slt $t0, $s1, $s0 
+            bne $t0, $zero, AesMayor 
+            sd $s1, C($zero) 
+            halt 
+AesCero:    sd $zero, C($zero) 
+            halt 
+AesMayor:   dmul $t1, $zero, 2 
+            sd $t1, C($zero) 
+            halt
+
+
+# INCISO C
+
+            .data 
+N:          .word 5 
+F:          .word 0 
+
+            .text 
+            ld $s0, N($zero) 
+            ld $s1, F($zero) 
+            daddi $s1, $zero, 1 
+            daddi $s2, $zero, 1 
+            LOOP: slt $t0, $s0, $s2 
+            bne $t0, $zero, FIN # Si N < i -> salir 
+            dmul $s1, $s1, $s2 # F = F * i 
+            daddi $s2, $s2, 1 # i:= i + 1 j 
+            j loop 
+FIN:        sd $s1, F($zero) halt
